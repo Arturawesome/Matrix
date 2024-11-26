90bd6fe45667dd45195be8d1542627bbab3c430b
ArturAwesome <nasyrovartur151998@gmail.com>
2024-11-26 13:27:44 +0300

    Start work with app

--- matrix_app/include/mainwindow.h
+++ matrix_app/include/mainwindow.h
@@ -1,8 +1,24 @@
 // mainwindow.h
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-class MainWindow {
-public:
-    MainWindow();
+
+#include<QMAinWindow>   // standart Qt6 class dedicated
+                        // to creating the main window
+
+/*
+    when mainwindow.ui is created, Qt6 automatically generate the ui_mainwindow.h file with     interface of class.
+    For avoiding of interfaces conflict  we put this class in Ui object;
+*/
+namespace Ui{
+    class MAinWindow; //
+}
+
+
+// My class for logic of my widnow application/
+class MainWindow: public QMAinWindow{
+    Q_OBJECT
 };
+
+
+
 #endif // MAINWINDOW_H
